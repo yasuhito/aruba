34e9413f981e8f9cc18725da8af1771e9695b596
Dennis Günnewig <dg1@ratiodata.de>
2015-06-02 11:31:19 +0200

    Move some methods to core module

--- lib/aruba/api.rb
+++ lib/aruba/api.rb
@@ -101,59 +101,6 @@ module Aruba
       Dir.glob(expand_path('**/*'))
     end
 
-    # @deprecated
-    # @private
-    def in_current_dir(*args, &block)
-      warn('The use of "in_current_dir" is deprecated. Use "in_current_directory" instead')
-      in_current_directory(*args, &block)
-    end
-
-    # Clean the current directory
-    def clean_current_directory
-      _rm_rf(current_directory)
-      _mkdir(current_directory)
-    end
-
-    # @deprecated
-    # @private
-    def clean_current_dir(*args, &block)
-      warn('The use of "clean_current_dir" is deprecated. Use "clean_current_directory" instead')
-      clean_current_directory(*args, &block)
-    end
-
-    # Get access to current dir
-    #
-    # @return
-    #   Current directory
-    def current_directory
-      File.join(*dirs)
-    end
-
-    # @deprecated
-    # @private
-    def current_dir(*args, &block)
-      warn('The use of "current_dir" is deprecated. Use "current_directory" instead')
-      current_directory(*args, &block)
-    end
-
-    # Switch to directory
-    #
-    # @param [String] dir
-    #   The directory
-    def cd(dir)
-      dirs << dir
-      raise "#{current_directory} is not a directory." unless File.directory?(current_directory)
-    end
-
-    # The path to the directory which should contain all your test data
-    # You might want to overwrite this method to place your data else where.
-    #
-    # @return [Array]
-    #   The directory path: Each subdirectory is a member of an array
-    def dirs
-      @dirs ||= Aruba.config.current_directory
-    end
-
     # Create a file with given content
     #
     # The method does not check if file already exists. If the file name is a
